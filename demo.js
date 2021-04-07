7e7cfa17d63939e7d3140fa0c0e81d29e691cffb
Tom Wilson <twilson63@gmail.com>
2021-04-07 20:24:30 +0000

    added command script

--- demo.js
+++ demo.js
@@ -4,8 +4,8 @@ const { data } = hyper(process.env.HYPER)
 
 async function main() {
   //const res = await data('movie').post({id: '1', title: 'Ghostbusters'})
-  //const res = await data('movie').get('1')
-  //console.log(res)
+  const res = await data('movie').get('1')
+  console.log(res)
 }
 
 main()
