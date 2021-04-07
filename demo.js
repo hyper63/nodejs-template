52b82cb16b5d98428d6fd022d3daaafc47649239
Tom Wilson <twilson63@gmail.com>
2021-04-07 20:19:02 +0000

    first commit

---
+++ demo.js
@@ -0,0 +1,11 @@
+import { hyper } from '@hyperio/client'
+
+const { data } = hyper(process.env.HYPER)
+
+async function main() {
+  //const res = await data('movie').post({id: '1', title: 'Ghostbusters'})
+  //const res = await data('movie').get('1')
+  //console.log(res)
+}
+
+main()
